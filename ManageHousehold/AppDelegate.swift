4206ed2c60b75b56dd43472755a24b98b576d96e
Naoya Ishida <>
2019-09-30 23:41:38 +0900

    create project

---
+++ ManageHousehold/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ManageHousehold
+//
+//  Created by Ishida Naoya on 2019/09/23.
+//  Copyright © 2019 Ishida Naoya. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ ManageHousehold/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManageHousehold
+//
+//  Created by Ishida Naoya on 2019/09/23.
+//  Copyright © 2019 Ishida Naoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
